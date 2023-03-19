209780414b27d1c478481db6f9f4c2c940b0d933
serezha3179 <sergeiabramkin.79@gmail.com>
2023-03-19 19:29:34 +0300

    commit 1

---
+++ gulp/config/path.js
@@ -0,0 +1,36 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        js: `${srcFolder}/js/app.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        fonts: `${srcFolder}/fonts/**/*.*`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+        fonts: `${srcFolder}/fonts/**/*.*`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder
+
+}
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from './modules/functions.js';
+
+flsFunctions.testWebP(function (support) {
+    
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }else{
+    document.querySelector('body').classList.add('no-webp');
+    }
+}); 
